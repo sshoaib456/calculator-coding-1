2c36f1fa5047527b8a7d951d73f5d6d8554500ea
Syed Shoaib UR Rehman <86778601+sshoaib456@users.noreply.github.com>
2021-09-09 00:54:58 -0700

    Add files via upload

---
+++ python.py
@@ -0,0 +1,48 @@
+-
+
+
+
+
+
+
+def addition (num1,num2):
+  result= num1 + num2
+  print(result)
+num1=10
+num2=5
+
+value = addition (num1,num2)
+print(value)
+
+def sub (num1,num2):
+    result= num1 - num2
+    print(result)
+
+value = sub (num1,num2)
+print(value)
+
+def multiplication (num1,num2):
+    result= num1 * num2
+    print(result)
+
+value = multiplication (num1,num2)
+print(value)
+
+def div (num1,num2):
+    result= num1 % num2
+    print(result)
+
+value = div (num1,num2)
+print(value)
+
+num1= int(input ( ))
+num1= int(input ( ))
+
+"""# New Section"""
+
+
+
+
+
+
+
